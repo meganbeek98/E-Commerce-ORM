ca4303b743286e12521b459f17fb6da1f46861f5
meganbeek98 <meganbeek98@gmail.com>
2022-07-20 01:44:18 -0400

    models folder edits

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,15 +1,48 @@
-// import important parts of sequelize library
+// import sequelize library
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
+// import the database connection from config.js
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
+// set up product model
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
@@ -21,3 +54,4 @@ Product.init(
 );
 
 module.exports = Product;
+
