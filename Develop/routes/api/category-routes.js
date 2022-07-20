bbd636502df6f09dac923f37cf42304c8f7351b2
meganbeek98 <meganbeek98@gmail.com>
2022-07-20 02:11:09 -0400

    get new product

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -34,12 +34,59 @@ router.get('/:id', (req, res) => {
 });
 
 // create a new category
+router.post('/', (req, res) => {
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((cData) => res.json(cData))
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
 
 // update a category by its `id` value
-
-
-
+router.put('/:id', (req, res) => {
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((cData) => {
+      if (!cData) {
+        res.status(404).json({ message: 'Category id not found' });
+        return;
+      }
+      res.json(cData);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
 // delete a category by its `id` value
+router.delete('/:id', (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((cData) => {
+      if (!cData) {
+        res.json(404).json({ message: 'Category id not found' });
+        return;
+      }
+      res.json(cData);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
+
+module.exports = router;
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,16 +4,26 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const pData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(pData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+// find all products
+
+
+
 // get one product
-router.get('/:id', (req, res) => {
+
   // find a single product by its `id`
+
   // be sure to include its associated Category and Tag data
-});
 
 // create new product
 router.post('/', (req, res) => {
