a87e7e0a01b4b9880a187104efe4d8100a3d1b14
meganbeek98 <meganbeek98@gmail.com>
2022-07-20 02:24:33 -0400

    update tag-routes, find all + create new

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,26 +3,36 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+// find all tags  // be sure to include its associated Product data
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [Product],
+  })
+    .then((tagData) => res.json(tagData))
+    .catch((err) => res.status(500).json(err));
 });
 
+// find a single tag by its `id`   // be sure to include its associated Product data
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
 });
 
+// create a new tag
 router.post('/', (req, res) => {
-  // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name,
+  })
+    .then((tagData) => res.json(tagData))
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
+// update a tag's name by its `id` value
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
 });
 
+// delete on tag by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
 });
 
 module.exports = router;
