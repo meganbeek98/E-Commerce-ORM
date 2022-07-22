fcb08f3a8aae0b269223864f0f2871a940ea0fea
meganbeek98 <meganbeek98@gmail.com>
2022-07-22 10:25:45 -0400

    final attempt

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,19 @@
+-- Active: 1658489379421@@localhost@3306@ecommerce_db
 -- DROP DATABASE
 DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
+
+USE ecommerce_db;
+
+-- CREATE DATABASE
+
+CREATE TABLE category;
+CREATE TABLE product;
+
+CREATE TABLE product_tag;
+
+CREATE TABLE products_table;
+
+
